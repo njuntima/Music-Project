d2319e9a17ea4d8f63b20f232b7a8e37e03fe829
condoowner0607 <condoowner0607@gmail.com>
2025-04-03 11:35:31 -0400

    Phase 2 code

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
+import mysql.connector
+from mysql.connector import Error
 
 app = Flask(__name__)
+
 @app.route('/')
 def db():
-    return "db project"
+    try:
+        connection = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="jasper0607",
+            database="CS4604_Crappy_Spotify_Clone"
+        )
+
+        return render_template('index.html', dbstatus="DB Connected")
+
+    except Exception as e:
+
+        return render_template('index.html', dbstatus="DB Connection Failed")
 
 if __name__ == '__main__':
     app.run()
